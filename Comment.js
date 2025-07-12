7b72076a1e175d9c15aee68806c9941f1f4d6c4e
Aamer32 <aamer.abrak12@gmail.com>
2025-07-12 14:56:43 +0100

    Create Comment.js

---
+++ Comment.js
@@ -0,0 +1,15 @@
+import React, {component} from 'react'
+
+export default class Comments extends Component {
+  constructor(propos)  {
+    super(props);
+    this.commentBox = React.creatRef();
+  }
+componentDidMount() {
+<script src="https://utteranc.es/client.js"
+        repo="[ENTER REPO HERE]"
+        issue-term="og:title"
+        theme="github-light"
+        crossorigin="anonymous"
+        async>
+</script>
